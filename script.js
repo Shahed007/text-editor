db3552365f056e50832c6531d0528753bb6be887
Shahed <sk0178397@gmial.com>
2023-12-29 15:00:31 +0600

    Created font size input and color input.

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-const fonts = ["lato", "inter", "robot"];
+const fonts = ["inter", "montserrat", "lato", "open sans", "roboto"];
 
 const font = document.getElementById("font");
 
 fonts.forEach((element) => {
   const option = document.createElement("option");
   option.value = element;
-  option.innerText = element;
+  option.innerText = element.charAt(0).toUpperCase() + element.slice(1);
   font.appendChild(option);
 });
